0d9596477f2e91c1082991b745189919d5d20d96
Brett Bender <brett@bearded.com>
2016-05-19 14:08:11 -0400

    Fix modal dialog z-index problem, fix modal dialog focus when opening / closing

--- source/assets/javascripts/modal.js
+++ source/assets/javascripts/modal.js
@@ -2,17 +2,15 @@ var $modalTrigger = $('.modal-button'),
     $modalContent = $('.modal-content'),
     $modalWrapper = $('.modal-wrapper'),
     $modalClose = $('.close-modal'),
+    $modalTitle = $('#modalTitle');
     $mainContent = $('.main-content');
 
 function setModalButton() {
   $modalTrigger.on("click", function(e){
     $mainContent.attr('aria-hidden', 'true');
-    $mainContent.attr('tabindex', '-1');
     $modalWrapper.attr('aria-hidden', 'false');
-    $modalTrigger.blur();
-    $modalClose.focus();
-    e.preventDefault();
     $modalWrapper.css('display', 'block');
+    $modalTitle.focus();
     deflectFocus();
   });
 };
@@ -22,16 +20,15 @@ function setModalClose() {
     $modalWrapper.attr('aria-hidden', 'true');
     $modalWrapper.css('display', 'none');
     $mainContent.attr('aria-hidden', 'false');
-    $modalClose.focus();
-    e.preventDefault();
+    $modalTrigger.focus();
+    $mainContent.off('.modalOpen');
   });
 };
 
 function deflectFocus() {
-  $mainContent.on('focusin', function(e) {
+  $mainContent.on('focusin.modalOpen', function(e) {
     console.log('deflect focus working');
-    $modalWrapper.focus();
-    e.preventDefault();
+    $modalClose.focus();
   });
 };
 
