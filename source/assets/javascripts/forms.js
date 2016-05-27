a1f89efb6c7817c2aceb9c366fbbd911b26cb57b
Lindsay Silver <lindsay@bearded.com>
2016-05-27 11:36:33 -0400

    Making forms a bit better--adding some new javascript that calls out aria-live attributes for forms

--- source/assets/javascripts/forms.js
+++ source/assets/javascripts/forms.js
@@ -1,17 +1,27 @@
 function handleFormSubmission(){
+  $('.form-example input').on('keyup', function(e){
+    $(this).addClass('edited');
+  });
+
   $('.form-example [type="submit"]').on('click.formValidation', function(e){
     shouldPrevent = false;
+    errorList = [];
+
 
     $form = $(this).parents('form');
     $inputs = $form.find('input[required]');
 
     $inputs.each(function(index, input){
       errorMessageSelector = 'label[for="' + $(input).attr('id') + '"] .error-message';
+      $form.find(errorMessageSelector).removeAttr('aria-live');
       error = $form.find(errorMessageSelector);
+      error.css('display', 'none');
 
       if(!input.validity.valid){
         error.css('display', 'inline-block');
-        shouldPrevent = true;
+        shouldPrevent = true;        
+        errorList.push(error);
+        
       }
       else {
         error.css('display', 'none');
@@ -20,6 +30,7 @@ function handleFormSubmission(){
 
     if(!$form[0].checkValidity()){
       e.preventDefault();
+      errorList[0].attr('aria-live', 'assertive');
     }
   });
 }
